a10b98d635f7b0d73b037722cb46e96275047e43
CorderoPonce <127542303+CorderoPonce@users.noreply.github.com>
2023-04-26 22:45:10 -0400

    Add files via upload
    Menu main

--- main (2).c
+++ main (2).c
@@ -19,12 +19,52 @@ typedef struct Jugador{
 } Jugador;
 
 int main(){
+  HashMap *map = createMap(10000);
+  char nombre[31];
+  char item[31];
+  unsigned short numIngresado;
+  int largoName;
 
+  while (true) {
 
+    scanf("%hu", &numIngresado);
+    while (numIngresado > 9 || numIngresado < 0) {
+      printf("Ingrese un número válido \n");
+      scanf("%hu", &numIngresado);
+    }
+    if (numIngresado == 0) {
+      printf("\n");
+      puts(BARRA);
+      printf("Fin De La Aventura!!!!!\n");
+      puts(BARRA);
+      return 0;
+    }
+    if (numIngresado == 1) {
+    }
 
+    if (numIngresado == 2) {
+    }
+    if (numIngresado == 3) {
+      
+    }
+    if (numIngresado == 4) {
 
+    }
+    if (numIngresado == 5) {
 
+    }
+    if (numIngresado == 6) {
 
+    }
+    if (numIngresado == 7) {
 
-  return 0;
+    }
+    if (numIngresado == 8) {
+
+    }
+    if (numIngresado == 9) {
+
+    } 
+
+  }
 }
