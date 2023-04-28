8d67bc51b18a06b41e56adedc41fde79f27b9c35
Nemeyas <127501439+Nemeyas@users.noreply.github.com>
2023-04-27 22:00:48 -0400

    Add files via upload
    Implemento la función gets_csv_field

--- main (2).c
+++ main (2).c
@@ -159,6 +159,45 @@ void mostrarJugadoresItemEsp(HashMap *map, List *nombres, char *nombre) {
     printf("No se encontraron jugadores con ese item\n");
 }
 
+const char *get_csv_field (char * tmp, int k) {
+    int open_mark = 0;
+    char* ret=(char*) malloc (100*sizeof(char));
+    int ini_i=0, i=0;
+    int j=0;
+    while(tmp[i+1]!='\0'){
+
+        if(tmp[i]== '\"'){
+            open_mark = 1-open_mark;
+            if(open_mark) ini_i = i+1;
+            i++;
+            continue;
+        }
+
+        if(open_mark || tmp[i]!= ','){
+            if(k==j) ret[i-ini_i] = tmp[i];
+            i++;
+            continue;
+        }
+
+        if(tmp[i]== ','){
+            if(k==j) {
+               ret[i-ini_i] = 0;
+               return ret;
+            }
+            j++; ini_i = i+1;
+        }
+
+        i++;
+    }
+
+    if(k==j) {
+       ret[i-ini_i] = 0;
+       return ret;
+    }
+
+    return NULL;
+}
+
 void importar(HashMap *map,FILE *archivo){
   char linea[1300];
   char *aux;
